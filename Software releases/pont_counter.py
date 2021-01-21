53f6f9a88292656e3f1c798238017ddc27ee395e
Vincenzo Deluca <delucaavincenzo@gmail.com>
2021-01-21 19:38:19 +0100

    Create pont_counter.py

---
+++ Software releases/pont_counter.py
@@ -0,0 +1,95 @@
+version = "1.0"
+add = 1
+A_Point = '0'
+B_Point = '0'
+print("Versione:", version)
+while True:
+    input = '0'
+    del(input)
+    print('A=', A_Point, 'B=', B_Point)
+    input = input('> ')
+
+    if input == 'help':
+        print("")
+        print("Aggiungi --[argomento]")
+        print("")
+        print("Scegli la sezione che ti interessa")
+        print("--add_point = Guida al aggiungere punti")
+        print("--remove_point = Guida al rimuovere punti")
+        print("")
+        continue
+
+    if input == 'help --add_point':
+        print("")
+        print("Aggiungere punti")
+        print("")
+        print("Per aggiugere un punto ad una squadra digita la lettera assegnata alla squadra")
+        print("")
+        print("ESEMPIO")
+        print("a")
+        print("")
+        continue
+
+    if input == 'help --remove_point':
+        print("")
+        print("Rimuovere punti")
+        print("")
+        print("Per rimuovere un punto ad una squadra digita la lettera assegnata alla squadra seguita da un -")
+        print("")
+        print("ESEMPIO")
+        print("b-")
+        print("")
+        continue
+
+    if input == "A":
+        A_Point = float(A_Point) + float(1)
+        continue
+
+    if input == "B":
+        B_Point = float(B_Point) + float(1)
+        continue
+
+    if input == "a":
+        A_Point = float(A_Point) + float(1)
+        continue
+
+    if input == "b":
+        B_Point = float(B_Point) + float(1)
+        continue
+
+    if input == "ab":
+        B_Point = float(B_Point) + float(1)
+        A_Point = float(A_Point) + float(1)
+        continue
+
+
+    if input == "A-":
+        A_Point = float(A_Point) - float(1)
+        continue
+
+    if input == "B-":
+        B_Point = float(B_Point) - float(1)
+        continue
+
+    if input == "a-":
+        A_Point = float(A_Point) - float(1)
+        continue
+
+    if input == "b-":
+        B_Point = float(B_Point) - float(1)
+        continue
+
+    if input == "ab-":
+        B_Point = float(B_Point) - float(1)
+        A_Point = float(A_Point) - float(1)
+        continue
+
+
+    if input == "AB":
+        B_Point = float(B_Point) + float(1)
+        A_Point = float(A_Point) + float(1)
+        continue
+
+    else:
+        print("Comando errato, digita help per informazioni sul utilizzo del software")
+
